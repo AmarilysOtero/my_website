896c92be62338813c69b494879fc696421fe8e2b
Amarilys Otero <amarilys.otero@upr.edu>
2022-04-03 20:54:44 -0400

    added programs

--- pg_one.js
+++ pg_one.js
@@ -1,1 +1,19 @@
-alert("Let's see some coding..!")
+
+var newlabel = document.createElement("Username");
+newlabel.setAttribute("for",id_from_input);
+newlabel.innerHTML = "Here goes the text";
+parentDiv.appendChild(newlabel);
+
+function bodyMassCalculator(w,h){
+    var bmi = w / Math.pow(h, 2);
+    return bmi;
+}
+
+function myFunction() {
+    document.getElementById("username").innerHTML = "Paragraph changed.";
+  }
+
+var bodymi= bodyMassCalculator(65, 1.8);
+console.log(bodymi);
+document.write(bodymi);
+
