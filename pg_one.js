6ecc2db99b9229259f91d0cd0cea224ac10f3963
Amarilys Otero <amarilys.otero@upr.edu>
2022-04-03 23:08:38 -0400

    bmi calcultor working properly

--- pg_one.js
+++ pg_one.js
@@ -1,19 +1,22 @@
-
-var newlabel = document.createElement("Username");
-newlabel.setAttribute("for",id_from_input);
-newlabel.innerHTML = "Here goes the text";
-parentDiv.appendChild(newlabel);
+const txt1 = document.getElementById('tbuser1');
+const txt2 = document.getElementById('tbuser2');
+const btn1 = document.getElementById('btn1');
+const out1 = document.getElementById('output1');
 
 function bodyMassCalculator(w,h){
-    var bmi = w / Math.pow(h, 2);
-    return bmi;
+  var bmi = w / Math.pow(h, 2);
+  return Math.round(bmi);
+}
+function outputResult(){
+  var bmo = bodyMassCalculator(txt1.value, txt2.value);
+  console.log(bmo);
+  out1.innerHTML= bmo;
 }
 
-function myFunction() {
-    document.getElementById("username").innerHTML = "Paragraph changed.";
-  }
+btn1.addEventListener('click',outputResult);
 
-var bodymi= bodyMassCalculator(65, 1.8);
-console.log(bodymi);
-document.write(bodymi);
+
+// var bodymi= bodyMassCalculator(65, 1.8);
+// console.log(bodymi);
+// document.write(bodymi);
 
