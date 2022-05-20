c2c9ecd3ae38ede4e03bced536c0cbd452367511
Amarilys Otero <amarilys.otero@upr.edu>
2022-05-20 16:07:40 -0400

    CV to Resume functions added

--- cv.js
+++ cv.js
@@ -1,62 +1,155 @@
-function addNewField(){
-    let newNode = document.createElement("textarea");
-    newNode.classList.add(" ")
+// Classes for a curriculum vitae
+class profile_data{
+    constructor(name, hometown, email, title){
+        this.name = name;
+        this.homeTown = hometown;
+        this.email = email;
+        this.title = title;
+    }
+}
+class objetive_data{
+    constructor(p){
+        this.paragraph = p;
+    }
+}
+class education_data{
+    constructor(date, degree, name, place, courses){
+        this.date = date;
+        this.degree = degree;
+        this.name = name;
+        this.place = place;
+        this.courses = courses;
+    }
+}
+class experience_data{
+    constructor(date, position, name, place, chores){
+        this.date = date;
+        this.position = position;
+        this.name = name;
+        this.place = place;
+        this.chores = chores;
+    }
 }
 
-class curriculumv{
+class extracurricular_data{
+    constructor(position, asociation_name, place, activities){
+        this.position = position;
+        this.aname = asociation_name;
+        this.place = place;
+        this.act = activities;
+    }
+}
+class skills_data{
+    constructor(skill){
+        this.skill = skill
+    }
+}
 
-    #first_name;
-    #last_name;
-    #email;
-    #adress;
-    #school;
-    #work;
 
-    constructor(){
-        this.#name = "Amarilys Joan Otero Ayala";
-        this.#email = "amarilys.otero@upr.edu";
-        this.#adress = "Bo. Quebradas Carr. 375, Yauco PR";
+// CV ----> Resume
+
+// function to retrieve elements from the resume.json file
+function resumeTemplate(element){
+    return `
+    <div class="profile">
+       <h2> ${element.ProfileName}</h2>
+       <h3> ${element.email}</h3>
+       <h3> ${element.homeTown}</h3>
+       <h3> ${element.title}</h3>
+       <h3> ${element.education}</h3>
+       <h3> ${element.education}</h3>
+       <h3> ${element.experience}</h3>
+       <h4> ${element.experience[0].date}</h4>
+       <h4> ${element.experience[1].position}</h4>
+       <h4> ${element.experience[2].name}</h4>
+       <h4> ${element.experience[3].chores}</h4>
+       <h3> ${element.extracurricular}</h3>
+       <h3> ${element.skills}</h3>
+       <h3> ${element.references}</h3>
+    </div>
+    `
+ }
+
+ // Browsing for a JSON File
+ async function getJSONFILE(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(xhttp.responseText);
+            var resume = response.resume;
+            var output = ''
+            for (var i = 0; i <resume.length; i++){
+                output += '<li>' +resume[i].ProfileName+'</li>';
+         }
+        
+        document.getElementById('resume').innerHTML = `${resume.map(resumeTemplate).join('')}`
+        display_data(resume)
+    }
+};
+    xhttp.open("GET", "resume.json", true);
+    xhttp.send();
+ }
+function printResume(){   
+    var element = document.getElementById('resume')
+    html2pdf(element); 
+}  
+
+// Browsing for a JSON File
+// async function display() {
+
+//     const requestURL = '/resume.json';
+//     const request = new Request(requestURL);
+
+//     const response = await fetch(request);
+//     const resume = await response.json();
+    
+//     display_data(resume)
+
+//   }
+
+  // Display in HTML
+function display_data(entry){
+    const section = document.querySelector('section');
+    const profile = new profile_data(entry.ProfileName, entry.homeTown, entry.email, entry.title);
+    const obj = new objetive_data(entry.title);
+    var ed = new education_data(entry.date, entry.degree, entry.name, entry.place, entry.courses);
+    var ex = new experience_data(entry.date, entry.position, entry.name, entry.place, entry.chores);
+    var extra = new extracurricular_data(entry.position, entry.asociation_name, entry.place, entry.activities)
+    var skill = new skills_data(entry.skill)
+    const cvArticle = document.createElement('article');
+    const myList = document.createElement('ul');
+
+
+    document.getElementById("profile").innerHTML = profile.ProfileName;
+    document.getElementsById("objv").innerHTML = obj.title;
+    document.getElementById("ed-date").innerHTML = ed.education[0];
+    document.getElementById("ed-degree").innerHTML = ed.education[1];
+    document.getElementById("ed-name").innerHTML = ed.education[2];
+    document.getElementById("ed-place").innerHTML = ed.education[3];
+    document.getElementById("ed-course").innerHTML = ed.education[4];
+    const Courses = entry.courses;
+      for (const course of Courses) {
+        const listItem = document.createElement('li');
+        listItem.textContent = course;
+        myList.appendChild(listItem);
+      }
+    document.getElementById("ex-date").innerHTML = ex.experience[0];
+    document.getElementById("ex-degree").innerHTML = ed.experience[1];
+    document.getElementById("ex-name").innerHTML = ed.experience[2];
+    document.getElementById("ex-place").innerHTML = ed.experience[3];
+    document.getElementById("ex-course").innerHTML = ed.experience[4];
+    const Chores = entry.chores;
+      for (const chores of Chores) {
+        const listItem1 = document.createElement('li');
+        listItem1.textContent = chores;
+        myList.appendChild(listItem1);
       }
 
-    objetive(){
-        console.log("Be part of a dynamic team where I can put into practice all my knowledge, achieve the overall goals of the team and the responsibilities in my charge, with the opportunity \n");
-        console.log("to continue developing in the area of work, personnel and intellectual field. \n");
+      cvArticle.appendChild()
+      cvArticle.appendChild(listItem);
+      cvArticle.appendChild(listItem1);
 
-    }
-
-    education(){
-        console.log("AUGUST 2021 - TO PRESENT")
-        console.log("BS COMPUTER SCIENCE, " + this.#school + "\n");
-
-    }
-    experience(){
-        console.log("AUGUST 2019 - DECEMBER 2019")
-        console.log("CUSTOMER SERVICE, " + this.#work + "\n");
-    }
-    skills(){
-        console.log("JAVA");
-        console.log("C++");
-        console.log("HTML");
-        console.log("CSS");
-    }
-
-    extracurricular(){
-        console.log("RUMblebots");
-    }
-    references(){
-      console.log("Available upon request.");
-    }
-
-    get getFname(){
-        return this.#first_name;
-    }
-    get getLname(){
-        return this.#last_name;
-    }
-    get getEmail(){
-        return this.#email;
-    }
-
+      section.appendChild(cvArticle);
 }
 
-var mydata = JSON.parse(cv);
+getJSONFILE();
